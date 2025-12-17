5051ff2d877daec99deac9a9d805337196e9dc3b
Marc V. <marckarella13@gmail.com>
2025-12-16 19:10:49 -0800

    moving stations.

--- passgen.py
+++ passgen.py
@@ -3,14 +3,22 @@ import secrets
 
 LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz"
 UPPERCASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-NUMBER_CHARS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+NUMBER_CHARS = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
 SYMBOL_CHARS = "! @ # $ % ^ & * ( ) _ + | ; : < > , . / ?`~[]=-"
 
 
-def validate_params(length: int, use_lowercase: bool, use_uppercase: bool, use_numbers: bool, use_symbols: bool,) -> bool:
+def validate_params(
+    length: int,
+    use_lowercase: bool,
+    use_uppercase: bool,
+    use_numbers: bool,
+    use_symbols: bool,
+) -> bool:
     """Validate password generation parameters"""
 
-    if 8 <= length <= 128 and (use_lowercase or use_uppercase or use_numbers or use_symbols):
+    if 8 <= length <= 128 and (
+        use_lowercase or use_uppercase or use_numbers or use_symbols
+    ):
         return True
 
     else:
@@ -19,14 +27,38 @@ def validate_params(length: int, use_lowercase: bool, use_uppercase: bool, use_n
         )
 
 
-def build_char_pool(use_lowercase: bool, use_uppercase: bool, use_numbers: bool, use_symbols: bool) -> str:
+def build_char_pool(
+    use_lowercase: bool, use_uppercase: bool, use_numbers: bool, use_symbols: bool
+) -> str:
     """build a string based on user preference."""
-    pass
+    use_lowercase = LOWERCASE_CHARS
+    use_uppercase = UPPERCASE_CHARS
+    use_numbers = NUMBER_CHARS
+    use_symbols = SYMBOL_CHARS
 
-def generate_password(length: int = 16, use_lowercase: bool = True, use_uppercase: bool = True, use_numbers: bool = True, use_symbols: bool = True) -> str | None:
+    chars = use_uppercase or use_lowercase
+    nums_symbols = use_numbers or use_symbols
+
+    return chars or nums_symbols
+
+
+def generate_password(
+    length: int = 16,
+    use_lowercase: bool = True,
+    use_uppercase: bool = True,
+    use_numbers: bool = True,
+    use_symbols: bool = True,
+) -> str | None:
     """Generate a cryptographically secure random password."""
     pass
 
 
-if __name__ == '__main__':
-    print(validate_params(length=200, use_lowercase=False, use_numbers=True, use_symbols=False, use_uppercase=False))
+if __name__ == "__main__":
+    print(
+        build_char_pool(
+            use_lowercase=False,
+            use_numbers=False,
+            use_symbols=False,
+            use_uppercase=True,
+        )
+    )
