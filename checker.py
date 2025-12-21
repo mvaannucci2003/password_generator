87638476dd28e66251e4a91dde2122bc40d493a2
mvaannucci2003 <marckarella13@gmail.com>
2025-12-21 15:30:48 -0800

    completed the penalty function for the checker. Building the

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import string
-
+import re
+from collections import Counter
 # Total score: 0-100 points
 # Length score: 0-25 points
 # Variety score: 0-60 points (15 points per character type)
@@ -18,15 +19,18 @@ def calculate_length_score(password: str) -> int:
     pass_length = len(password)
 
     if 5 <= pass_length < 10:
-        return 5
+        pass_length = 5
     if 10 <= pass_length < 15:
-        return 10
+        pass_length = 10
     if 15 <= pass_length < 20:
-        return 15
+        pass_length = 15
     if 20 <= pass_length < 25:
-        return 20
+        pass_length = 20
     if pass_length >= 25:
-        return 25
+        pass_length = 25
+
+    return pass_length
+
 
 
 # Scoring rules:
@@ -68,7 +72,29 @@ def calculate_pattern_penalty(password: str) -> int:
     :return: Description
     :rtype: int
     """
-    pass
+    score = 0
+    dup = Counter(password)
+    SYMBOL_CHARS = string.punctuation
+
+    if re.findall(r"(.)\1", password):
+        score -= 5
+
+    if not any(c.islower() for c in password):
+        score -= 5
+    if not any(c.isupper() for c in password):
+        score -= 5
+    if not any(c.isdigit() for c in password):
+        score -= 5
+    if not any(c in SYMBOL_CHARS for c in password):
+        score -= 5
+
+    if [c for c, cnt in dup.items() if cnt > 1]:
+        score -= 5
+
+    return score
+
+
+
 
 
 def check_strength(password: str) -> int:
@@ -80,14 +106,32 @@ def check_strength(password: str) -> int:
     :return: Description
     :rtype: int
     """
-    pass
+    strength = dict(
+        score = 0,
+        rating = ['Very Weak', 'Weak', 'Medium', 'Strong', 'Very Strong'],
+        length = 0,
+        has_lower = True,
+        has_upper = True,
+        has_numbers = True,
+        has_symbols = True
+    )
+
+    if calculate_length_score(password):
+        pass
+        # TODO Calculate your functions by putting them in the sum function for the accumlated score
+        # TODO Determine the string rating based on the resulted score similar idea to the bool range check.
+        # TODO Check character types by again reusing the logic from check_variety
+        # TODO Return your DICT and complete the function.
+
 
 
 if __name__ == "__main__":
 
-    print(calculate_length_score(password="password"))
+    print(calculate_length_score(password="password123"))
 
-    print(calculate_variety_score(password="password"))
+    print(calculate_variety_score(password="passwor2"))
+
+    print(calculate_pattern_penalty(password="Fasonhs1@m"))
 
     # result = check_strength("password")
     # print(f"Password: password")
